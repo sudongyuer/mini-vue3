4e04ca410402c577728decf94b4b857f8675a5ea
sudongyu <976499226@qq.com>
2022-02-17 16:31:08 +0800

    feat: add creatApp

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-import {watchEffect,reactive}from './core/reactivity/reactivity.js'
-
-const a = reactive({
-    uname:"sudongyu"
-})
-window.a=a
-watchEffect(()=>{
-    document.body.innerText=''
-    document.body.innerText=a.uname
-})
+import App from './App.js'
+import { createApp } from './core/index.js'
+createApp(App).mount('#app')
