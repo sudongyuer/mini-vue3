af8cd51eccbfbe45a3bef8753e34bc01a49ff0b2
sudongyu <976499226@qq.com>
2022-02-28 08:29:52 +0800

    feat: add effect runner

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -6,7 +6,7 @@ class ReactiveEffect {
 
   run() {
     activeEffect = this;
-    this._fn();
+   return this._fn(); 
   }
 }
 
@@ -50,4 +50,6 @@ export function effect(fn) {
   const _effect = new ReactiveEffect(fn);
  
   _effect.run();
+
+  return _effect.run.bind(_effect)
 }
--- src/reactivity/tests/index.spec.ts
+++ src/reactivity/tests/index.spec.ts
@@ -1,4 +1,5 @@
 import { add } from "../.."
+import { effect } from "../effect"
 
 it("init",()=>{
     expect(true).toBe(true)
@@ -6,4 +7,16 @@ it("init",()=>{
 
 it("esm test",()=>{
     expect(add(1,2)).toBe(3)
+})
+
+it("should return runner when run effect",()=>{
+    let foo = 10;
+   const runner= effect(()=>{
+        foo++
+        return "foo"
+    })
+    expect(foo).toBe(11);
+    const r  = runner()
+    expect(foo).toBe(12)
+    expect(r).toBe("foo")
 })
