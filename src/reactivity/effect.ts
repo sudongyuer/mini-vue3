4ac598e4dc9a68b4cfc4a1e87f5e629fe4def9d9
sudongyu <976499226@qq.com>
2022-03-02 22:53:41 +0800

    feat: add effect scheduler

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,20 +1,21 @@
 class ReactiveEffect {
   private _fn: any;
-  constructor(fn) {
+  constructor(fn, public scheduler?) {
     this._fn = fn;
+    
   }
 
   run() {
     activeEffect = this;
-   return this._fn(); 
+    return this._fn();
   }
 }
 
 const targetMap = new Map();
 /**
  * 收集依赖
- * @param target 
- * @param key 
+ * @param target
+ * @param key
  */
 export function track(target, key) {
   //target -> key ->dep
@@ -33,23 +34,28 @@ export function track(target, key) {
 
 /**
  * 触发依赖
- * @param target 
- * @param key 
+ * @param target
+ * @param key
  */
 export function trigger(target, key) {
   let depsMap = targetMap.get(target);
   let dep = depsMap.get(key);
   for (const effect of dep) {
-    effect.run();
+    if (effect.scheduler) {
+      effect.scheduler();
+    } else {
+      effect.run();
+    }
   }
 }
 // 全局变量，用来保存当前正在执行的effect
 let activeEffect;
-export function effect(fn) {
+export function effect(fn, options:any={}) {
+  const scheduler = options.scheduler;
   //调用fn
-  const _effect = new ReactiveEffect(fn);
- 
+  const _effect = new ReactiveEffect(fn, scheduler);
+
   _effect.run();
 
-  return _effect.run.bind(_effect)
+  return _effect.run.bind(_effect);
 }
