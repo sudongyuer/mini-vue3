848aca27d49e39cdf8c2bb606a051df6f040a8a0
sudongyu <976499226@qq.com>
2022-03-02 22:54:01 +0800

    feat: add effect scheduler test

--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -1,17 +1,41 @@
 import { effect } from "../effect";
 import { reactive } from "../reactive";
 
-describe('effect',()=>{
-    it('happy path',()=>{
-        const user = reactive({
-            age :10
-        })
-        let nextAge;
-        effect(()=>{
-            nextAge = user.age +1
-        });
-        expect(nextAge).toBe(11)
-        user.age++;
-        // expect(nextAge).toBe(12)
-    })
-})
+describe("effect", () => {
+  it("happy path", () => {
+    const user = reactive({
+      age: 10,
+    });
+    let nextAge;
+    effect(() => {
+      nextAge = user.age + 1;
+    });
+    expect(nextAge).toBe(11);
+    user.age++;
+    expect(nextAge).toBe(12);
+  });
+
+  it("scheduler", () => {
+    let dummy;
+    let run: any;
+    const scheduler = jest.fn(() => {
+      run = runner;
+    });
+    const obj = reactive({ foo: 1 });
+    const runner = effect(
+      () => {
+        dummy = obj.foo;
+      },
+      { scheduler }
+    );
+    expect(scheduler).not.toHaveBeenCalled();
+    expect(dummy).toBe(1)
+    // should be called on first trigger
+    obj.foo++;
+    expect(dummy).toBe(1)
+    //manually run
+    run();
+    // should have run
+    expect(dummy).toBe(2);
+  });
+});
