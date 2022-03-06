4d23d3c9cac585c04d665fe6270869778b090e2a
sudongyu <976499226@qq.com>
2022-03-06 11:54:59 +0800

    feat: add hasChanged method

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -2,3 +2,7 @@ export const extend = Object.assign
   export function isObject(val){
       return val!==null &&(typeof val === 'object')
   }
+
+  export const hasChanged=(val,newValue)=>{
+    return !Object.is(val,newValue)
+  }
