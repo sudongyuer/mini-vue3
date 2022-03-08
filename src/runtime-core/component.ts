c6a34eea4533e37ec5cf987c76b99fc69441dc16
sudongyu <976499226@qq.com>
2022-03-08 23:57:31 +0800

    feat: add component

---
+++ src/runtime-core/component.ts
@@ -0,0 +1,42 @@
+export function createComponentInstance(vnode) {
+  const component = {
+    vnode,
+    type: vnode.type,
+  };
+  return component;
+}
+
+export function setupComponent(instance) {
+  //TODO
+  //initProps()
+  //initSlots()
+  setupStatefulComponent(instance);
+}
+// component -> vnode -> instance
+function setupStatefulComponent(instance: any) {
+  const Component = instance.type;
+  const { setup } = Component;
+
+  if (setup) {
+    //如果返回function 就是一个render函数 or Object 注册到组件上下文
+    const setupResult = setup();
+    handleSetupResult(instance, setupResult);
+  }
+}
+function handleSetupResult(instance, setupResult: any) {
+  //function Object
+  //TODO function
+
+  if (typeof setupResult === "object") {
+    instance.setupState = setupResult;
+  }
+
+  finishComponentSetup(instance);
+}
+
+function finishComponentSetup(instance: any) {
+  const Component = instance.type;
+  if (!Component.render) {
+    instance.render = Component.render;
+  }
+}
