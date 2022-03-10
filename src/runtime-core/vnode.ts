00c9eaede01db991b132d7c575331f46020674b4
sudongyu <976499226@qq.com>
2022-03-10 23:23:43 +0800

    chore: update vnode

--- src/runtime-core/vnode.ts
+++ src/runtime-core/vnode.ts
@@ -1,11 +1,24 @@
-export function createVNode(type,props?,children?){
-    const vnode ={
+import { ShapeFlags } from "../shared/ShapeFlags"
+
+export function createVNode(type, props?, children?) {
+    const vnode = {
         type,
         props,
         children,
-        el:null
+        el: null,
+        shapeFlag: getShapeFlag(type)
     }
+    if (typeof children === 'string') {
+        vnode.shapeFlag |= ShapeFlags.TEXT_CHILDREN
+    } else if (Array.isArray(children)) {
+        vnode.shapeFlag |= ShapeFlags.ARRAY_CHILDREN
+    }
+
     return vnode
 }
 
 
+
+function getShapeFlag(type: any) {
+    return typeof type === 'string' ? ShapeFlags.ELEMENT : ShapeFlags.STATEFUL_COMPONENT
+}
