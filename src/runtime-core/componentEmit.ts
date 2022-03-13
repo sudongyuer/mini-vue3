7bad00c93a327d7a9fc9ad1a263344d8e75c143a
sudongyu <976499226@qq.com>
2022-03-13 23:26:15 +0800

    chore: update shared

--- src/runtime-core/componentEmit.ts
+++ src/runtime-core/componentEmit.ts
@@ -1,3 +1,5 @@
+import { camelize, toHandlerKey } from "../shared";
+
 export const emit =(instance,event,...args)=>{
 
   console.log("emit",event);
@@ -12,16 +14,7 @@ export const emit =(instance,event,...args)=>{
       return str.charAt(0).toUpperCase() + str.slice(1);
   }
 
-  const camelize = (str)=>{
-    return str.replace(/-(\w)/g,(_,p)=>{
-      return p ? p.toUpperCase() : ''
-    })
-  }
-
-  const toHandlerKey =(str)=>{
-    return  str ? "on" + capitalize(str) : ""
-  }
-
+  
   const handler = props[toHandlerKey(camelize(event))]
 
   handler && handler(...args)
--- src/shared/index.ts
+++ src/shared/index.ts
@@ -8,3 +8,16 @@ export const extend = Object.assign
   }
 
   export const hasOwn = (val,key)=>Object.prototype.hasOwnProperty.call(val,key);
+
+  export const camelize = (str)=>{
+    return str.replace(/-(\w)/g,(_,p)=>{
+      return p ? p.toUpperCase() : ''
+    })
+  }
+  //去掉首字母
+  export const capitalize = (str) => {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+  export const toHandlerKey =(str)=>{
+    return  str ? "on" + capitalize(str) : ""
+  }
