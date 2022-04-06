1108f338277c2de8c914e3407c412888c1c80d05
sudongyu <976499226@qq.com>
2022-04-06 14:12:52 +0800

    chore: update

--- src/runtime-core/createApp.ts
+++ src/runtime-core/createApp.ts
@@ -1,12 +1,12 @@
 import { render } from "./renderer";
 import { createVNode } from "./vnode";
 
-export function createApp(rootComponent) {
+export function createApp(rootComponent,parentComponent) {
   return {
     mount(rootContainer) {
       const vnode = createVNode(rootComponent);
 
-      render(vnode, rootContainer);
+      render(vnode, rootContainer,parentComponent);
     },
   };
 }
