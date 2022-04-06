6836e970fcac6a867f3baae5b114ad3938bb1e60
sudongyu <976499226@qq.com>
2022-04-06 14:12:36 +0800

    feat: add apiInject

--- src/runtime-core/apiInject.ts
+++ src/runtime-core/apiInject.ts
@@ -1,8 +1,32 @@
-export function provide (key,value){
-  
+import { getCurrentInstance } from "./component";
+
+export function provide(key, value) {
+
+  const currentInstance: any = getCurrentInstance()
+
+  if (currentInstance) {
+    const { provides } = currentInstance
+    provides[key] = value
+
+  }
 
 }
 
-export function inject(key){
-  
+export function inject(key, defaultValue) {
+  const currentInstance: any = getCurrentInstance();
+
+  if (currentInstance) {
+    // const parentProvides = currentInstance.parent.provides;
+
+    // if (key in parentProvides) {
+    //   return parentProvides[key];
+    // }else if(defaultValue){
+    //   if(typeof defaultValue === "function"){
+    //     return defaultValue()
+    //   }
+    //   return defaultValue
+    // }
+    const parentProvides = currentInstance.parent.provides;
+    return parentProvides[key]
+  }
 }
